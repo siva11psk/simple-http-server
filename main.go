15723a381682eb35a7afdc8a06d3955af4a1216b
spalanisamy <sivakumar.palanisamy@endava.com>
2023-06-13 15:41:51 -0300

    Update handler to validate basic auth headers
    App has been updated to validate the username and password from the
    basic auth headers. Note that the username and password are fed into the
    application via system environment variables. The env vars APP_USERNAME
    and APP_PASSWORD should be set for the app to work.

--- main.go
+++ main.go
@@ -4,16 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome")
+type authentication struct {
+	userName string
+	password string
+}
+
+// populate the username and password from environment vars
+var appAuth = authentication{
+	userName: os.Getenv("APP_USERNAME"),
+	password: os.Getenv("APP_PASSWORD"),
 }
 
 func main() {
 
 	http.HandleFunc("/token", handler)
-
 	fmt.Println("Starting the http server")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(writer http.ResponseWriter, request *http.Request) {
+
+	userName, password, ok := request.BasicAuth()
+
+	if !ok {
+		fmt.Println("Unable to parse Auth info")
+		writer.WriteHeader(401)
+		return
+	}
+
+	if userName != appAuth.userName || password != appAuth.password {
+		fmt.Println("Authentication failed")
+		writer.WriteHeader(401)
+		return
+	}
+
+	fmt.Println("Authentication succeeded")
+	fmt.Fprintln(writer, "Welcome", userName)
+	writer.WriteHeader(200)
+	return
 }
